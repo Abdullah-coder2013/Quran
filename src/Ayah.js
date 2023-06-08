1f184eb22b3761685e7e0086b67a5800f2f61051
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2023-06-08 19:26:14 +0200

    update surah start

--- src/Ayah.js
+++ src/Ayah.js
@@ -1,10 +1,28 @@
 const Ayah = (props) => {
     if (props.page[props.index][1].includes("بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ")) {
+        let entryString = props.page[props.index][1];
+        let entryArray = entryString.split(" ");
+        let letter = entryArray.splice(3 + 1, entryArray.length - (3 + 1) );
+        if (letter[0] === "ٱلرَّحِيمِ") {
+            entryArray.push(letter);
+        }
+        let letterstogether = letter.join(" ");
+        const generateFirstAyah = () => {
+            if (letterstogether === "") {
+                return "";
+            }
+            else {
+                return `${letterstogether} (1)`;
+            }
+        }
         return (
             <>
-            <hr className="bg-slate-900"/>
-            <p className="font-extrabold text-6xl">{props.page[props.index][1]} ({props.number})</p>
-            <hr/>
+                <hr className="headAyahTop"/>
+                <div className="flex justify-center">
+                    <p className=" font-semibold text-5xl">❁ () {entryArray.join(" ")} ❁</p>
+                </div>
+                <hr className="headAyahBottom"/>
+                <span>{generateFirstAyah()}</span>
             </>
         )
     }
