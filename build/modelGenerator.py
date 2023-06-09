2ba689f9e386a3af366311f418b536ec877aa827
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2023-06-09 11:58:37 +0200

    update build

--- build/modelGenerator.py
+++ build/modelGenerator.py
@@ -1,22 +1,25 @@
+# IMPORTANT! This File could be brocken and send lot of incorrect data to the server #
+# Use CareFULLY!
+
 import requests
 import json
 
 totalayahs = []
 
-for i in range(604):
-    url = f"https://api.alquran.cloud/v1/page/{i + 1}/quran-uthmani"
+for i in range(10):
+    url = f"https://api.alquran.cloud/v1/page/{i + 1}/indopak"
     response = requests.get(url)
     data = response.json()
     pageayahs = []
     ayahs = []
     for ayah in data["data"]["ayahs"]:
-        ayahs.append([ayah["numberInSurah"],ayah["text"]])
+        ayahs.append([ayah["numberInSurah"],ayah["text"],ayah["surah"]["name"]])
     pageayahs.append(ayahs)
     # for ayah in pageayahs:
     #     totalayahs.append(ayah)
     print(pageayahs)
     for ayah in pageayahs:
-        with open(f"src/models/page-{i}.json", "w", encoding="utf-8") as f:
+        with open(f"models/page-{i}.json", mode="w", encoding="utf-8") as f:
             json.dump(ayah, f, ensure_ascii=False, indent=4)
     
     
