925a491ece4fbdc8d038e450da56f52297656c74
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2023-06-19 20:13:41 +0200

    make cover page not fade in

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,6 +2,8 @@ import Page from "./Page";
 import Cover from "./Cover";
 import TableOfContents from "./tableOfContents/TableOfContents";
 import swal from 'sweetalert'
+import MobileTurnPage from "./mobileTurnPage";
+import {isMobile} from 'react-device-detect';
 function Book(props) {
     
     const search = (event) => {
@@ -18,7 +20,7 @@ function Book(props) {
     }
     if (props.pg === -2){
         return (
-            <div className="tran">
+            <div>
                 <Cover setPg={props.setPg} bookmark={localStorage.getItem("bookmark")}/>
             </div>
         )
@@ -37,19 +39,43 @@ function Book(props) {
         localStorage.setItem("bookmark", props.pg);
         swal("Bookmark Added!", "You have successfully added the page as your bookmark.")
     }
-    return (
-        <div className="w-full items-center bg-slate-300 text-5xl max-lg:text-3xl text-right p-4 pb-8 rounded border-4 border-slate-400 shadow-lg">
-            
-            <div className="flex flex-start justify-center text-xl text-xss">
-                <button onClick={() => props.setPg(-2)} className="w-1/4 p-2 text-left hover:-translate-y-1 hover:scale-110 transition ease-in-out hover:text-green-800 font-bold">To Cover</button>          
-                <input type="number" className="w-1/4 p-2 border-0 outline-0 bg-slate-300 text-green-700" name="submit" onKeyDown={e => search(e)} placeholder="Search"/>
-                <input className="w-1/4 p-2 border-0 outline-0 text-center bg-slate-300 text-green-700" type="text" value={props.pg+1} readOnly/>
-                <button className="w-1/4 text-center p-2 hover:-translate-y-1 hover:scale-110 transition ease-in-out hover:text-green-800 font-bold" onClick={() => props.setPg(-1)}>Contents</button>          
-                <button onClick={setBookMark} className="w-1/4 p-2 text-right hover:-translate-y-1 hover:scale-110 transition ease-in-out hover:text-green-800 font-bold">Bookmark</button>
-            </div>
 
-            <Page pg={props.pg} />
-        </div>
-    );
+    if (isMobile === false && window.innerWidth <= 1023) {
+        return (
+            <div className="w-full items-center bg-slate-300 text-5xl max-lg:text-3xl text-right p-4 pb-8 rounded border-4 border-slate-400 shadow-lg">
+                
+                <div className="flex flex-start justify-center text-xl text-xss">
+                    <MobileTurnPage turnPage={"turn"} setPg={props.setPg} pg={props.pg}/>
+                    <button onClick={() => props.setPg(-2)} className="w-1/6 p-2 hover:-translate-y-1 hover:scale-110 transition ease-in-out hover:text-green-800 font-bold">To Cover</button>          
+                    <input type="number" className="w-1/6 p-2 border-0 outline-0 bg-slate-300 text-green-700" name="submit" onKeyDown={e => search(e)} placeholder="Search"/>
+                    <input className="w-1/6 p-2 border-0 outline-0 text-center bg-slate-300 text-green-700" type="text" value={props.pg+1} readOnly/>
+                    <button className="w-1/6 text-center p-2 hover:-translate-y-1 hover:scale-110 transition ease-in-out hover:text-green-800 font-bold" onClick={() => props.setPg(-1)}>Contents</button>          
+                    <button onClick={setBookMark} className="w-1/6 p-2 hover:-translate-y-1 hover:scale-110 transition ease-in-out hover:text-green-800 font-bold">Bookmark</button>
+                    <MobileTurnPage turnPage={"back"} setPg={props.setPg} pg={props.pg}/>
+                </div>
+    
+                <Page pg={props.pg} />
+            </div>
+        );
+    }
+
+    else if (!window.innerWidth <= 1023) {
+        return (
+            <div className="w-full items-center bg-slate-300 text-5xl max-lg:text-3xl text-right p-4 pb-8 rounded border-4 border-slate-400 shadow-lg">
+                
+                <div className="flex flex-start justify-center text-xl text-xss">
+                    <button onClick={() => props.setPg(-2)} className="w-1/4 p-2 text-left hover:-translate-y-1 hover:scale-110 transition ease-in-out hover:text-green-800 font-bold">To Cover</button>          
+                    <input type="number" className="w-1/4 p-2 border-0 outline-0 bg-slate-300 text-green-700" name="submit" onKeyDown={e => search(e)} placeholder="Search"/>
+                    <input className="w-1/4 p-2 border-0 outline-0 text-center bg-slate-300 text-green-700" type="text" value={props.pg+1} readOnly/>
+                    <button className="w-1/4 text-center p-2 hover:-translate-y-1 hover:scale-110 transition ease-in-out hover:text-green-800 font-bold" onClick={() => props.setPg(-1)}>Contents</button>          
+                    <button onClick={setBookMark} className="w-1/4 p-2 text-right hover:-translate-y-1 hover:scale-110 transition ease-in-out hover:text-green-800 font-bold">Bookmark</button>
+                </div>
+    
+                <Page pg={props.pg} />
+            </div>
+        );
+    }
+
+    
 }
 export default Book;
