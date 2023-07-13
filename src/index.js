00c5fdf1ca4ca39232cbd3c0997422cfec1ddee7
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2023-07-13 11:10:38 +0200

    make a proper service worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import { StrictMode } from 'react';
 import './index.css';
 import App from './App';
 
+if('serviceWorker' in navigator) {
+  let registration;
+
+  const registerServiceWorker = async () => {
+    registration = await navigator.serviceWorker.register('./service-worker.js');
+  };
+
+  registerServiceWorker();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<StrictMode>
     <App />
   </StrictMode>);
+
