21dad1b3b1f8ceb09cf2e9e8c788ddf85927803c
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2023-07-20 20:56:24 +0200

    update serviceworker?

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,9 @@
 // This is the "Offline copy of pages" service worker
+import workbox from 'https://storage.googleapis.com/workbox-cdn/releases/6.5.1/workbox-sw.js';
 
 const CACHE = "quran-offline";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.1/workbox-sw.js');
+
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
