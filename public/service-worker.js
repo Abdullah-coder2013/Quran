e42ed1e6f9d3d6b16b28585836d714056cb0a3a2
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2023-07-20 18:31:14 +0200

    update service worker

---
+++ public/service-worker.js
@@ -0,0 +1,39 @@
+//STORAGE OF BROWSER
+const CACHE_NAME = "quran-offline";
+const urlsToCache = ["/"];
+const self = this;
+
+//installation
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// listen for request
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return fetch(event.request).catch(() => caches.match("index.html"));
+    })
+  );
+});
+
+// actitivate the service worker
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)){
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import './index.css';
 import App from './App';
-
-serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<StrictMode>
