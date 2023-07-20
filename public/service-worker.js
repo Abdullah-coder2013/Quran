34330f6b0ccf1af09a28c1b5bb0afba1666684a1
Abdullah-coder2013 <93675484+Abdullah-coder2013@users.noreply.github.com>
2023-07-20 20:40:23 +0200

    updated workbox cdn to 6.5.1

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,39 +1,20 @@
-//STORAGE OF BROWSER
-const CACHE_NAME = "quran-offline";
-const urlsToCache = ["/"];
-const self = this;
+// This is the "Offline copy of pages" service worker
 
-//installation
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
+const CACHE = "quran-offline";
 
-      return cache.addAll(urlsToCache);
-    })
-  );
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.1/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// listen for request
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => {
-      return fetch(event.request).catch(() => caches.match("index.html"));
-    })
-  );
-});
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 
-// actitivate the service worker
-self.addEventListener("activate", (event) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
-    event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWhitelist.includes(cacheName)){
-                    return caches.delete(cacheName);
-                }
-            })
-        ))
-    )
-});
+console.log("Service Worker Activated");
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { StrictMode } from 'react';
 import './index.css';
 import App from './App';
 
+navigator.serviceWorker.register('/service-worker.js');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<StrictMode>
     <App />
